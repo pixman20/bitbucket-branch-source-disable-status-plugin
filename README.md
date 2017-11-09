1982fb85376885391e05699563029433efcd8bdf
Andrew Pixley <Andrew.Pixley@availity.com>
2017-11-09 20:45:09 +0000

    Adding option to disable bitbucket build status notifications

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# bitbucket-branch-source-disable-status-plugin
-bitbucket-branch-source-disable-status-plugin
+# Bitbucket Branch Source Disable Status Plugin
+
+This plugin adds a trait for the Bitbucket Branch Source plugin.
+It adds an option in the "Additional" section of the configuration to "Disable Bitbucket Build Status Notifications"
+
+
+## 1.0
+    - Adding option in Bitbucket Branch Source "Additional" section to disable Bitbucket Build Status Notifications
+
+
---
+++ src/main/java/jenkins/plugins/bitbucket/DisableNotificationsTrait.java
@@ -0,0 +1,89 @@
+/*
+ * The MIT License
+ *
+ * @author Andrew Pixley
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+package jenkins.plugins.bitbucket;
+
+import edu.umd.cs.findbugs.annotations.NonNull;
+import hudson.Extension;
+import jenkins.scm.api.SCMSource;
+import jenkins.scm.api.trait.SCMSourceContext;
+import jenkins.scm.api.trait.SCMSourceTrait;
+import jenkins.scm.api.trait.SCMSourceTraitDescriptor;
+import org.kohsuke.stapler.DataBoundConstructor;
+import com.cloudbees.jenkins.plugins.bitbucket.BitbucketSCMSourceContext;
+import com.cloudbees.jenkins.plugins.bitbucket.BitbucketSCMSource;
+
+/**
+ * A {@link SCMSourceTrait} that supresses all pull requests if the repository is public.
+ *
+ * @since 2.2.0
+ */
+public class DisableNotificationsTrait extends SCMSourceTrait {
+    /**
+     * Constructor.
+     */
+    @DataBoundConstructor
+    public DisableNotificationsTrait() {
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    protected void decorateContext(SCMSourceContext<?, ?> context) {
+        ((BitbucketSCMSourceContext) context).withNotificationsDisabled(true);
+    }
+
+    /**
+     * Our descriptor.
+     */
+    @Extension
+    public static class DescriptorImpl extends SCMSourceTraitDescriptor {
+        /**
+         * {@inheritDoc}
+         */
+        @NonNull
+        @Override
+        public String getDisplayName() {
+            return Messages.DisableNotificationsTrait_displayName();
+        }
+
+        /**
+         * {@inheritDoc}
+         */
+        @Override
+        public Class<? extends SCMSourceContext> getContextClass() {
+            return BitbucketSCMSourceContext.class;
+        }
+
+        /**
+         * {@inheritDoc}
+         */
+        @Override
+        public Class<? extends SCMSource> getSourceClass() {
+            return BitbucketSCMSource.class;
+        }
+
+    }
+}
